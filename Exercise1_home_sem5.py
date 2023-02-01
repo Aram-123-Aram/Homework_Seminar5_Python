335ab5db116820b68c787d33e65a62f8ae89a92e
Aram <armsir-8182@mail.ru>
2023-02-01 04:36:51 +0300

    delating the words in text,that contains'абв'

--- Exercise1_home_sem5.py
+++ Exercise1_home_sem5.py
@@ -1,6 +1,8 @@
 '''
 Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 '''
+#variant1
+'''
 text = input("Enter the text= ")
 list1 = text.split()
 print("list1= ", list1)
@@ -11,3 +13,12 @@ frig = "абв"
 list1 = [x for x in list1 if not frig in x]
 answer_text = " ".join(list1)
 print("answer_text =", answer_text)
+'''
+#variant2
+text = input('Enter the text= ')
+list1 = text.split()
+print(list1)
+list2 = list(filter(lambda x: not 'абв' in x, list1))
+print(list2)
+text_result = ' '.join(list2)
+print(text_result)
