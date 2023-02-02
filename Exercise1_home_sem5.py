358b96adbd5ea6f43d934cdac7623830d8cb1f19
Aram <armsir-8182@mail.ru>
2023-02-02 03:08:06 +0300

    How much candy will take first player to win the game!

--- Exercise1_home_sem5.py
+++ Exercise1_home_sem5.py
@@ -6,8 +6,6 @@
 text = input("Enter the text= ")
 list1 = text.split()
 print("list1= ", list1)
-#enum_list = list(enumerate(list1))
-#print("enum_list= ", enum_list)
 
 frig = "абв"
 list1 = [x for x in list1 if not frig in x]
--- Exercise2_home_sem5.py
+++ Exercise2_home_sem5.py
@@ -9,8 +9,29 @@ b) Подумайте как наделить бота ""интеллектом"
 '''
 n_candy = 2021
 import random
-player1=1
-player2=2
+
 player_actual = random.randint(1,2)
-if player_actual == player1:
-    m = 
+if player_actual == 1:
+    n_candy = 2021
+    n=28
+    xod_player1 = 0
+    xod_player2 = 0
+    while n_candy > 0:
+        xod_player1+=1
+        n_candy=n_candy-28
+        if n_candy > 0:
+            xod_player2+=1
+            n_candy=n_candy-28
+else:
+    n_candy = 1990
+    n=3
+    xod_player2 = 1
+    xod_player1 = 2
+    while n_candy > 0:
+        xod_player2+=1
+        n_candy=n_candy-28
+        if n_candy > 0:
+            xod_player1+=1
+            n_candy=n_candy-28
+if xod_player1>xod_player2:
+    print(f"Первому играку нужно взять {n} конфет!{player_actual,xod_player1, xod_player2}")
